39bf0343a57d849f8e43240cdf18c8d61db1e224
najmulh112233-crypto <najmulh112233@gmail.com>
2025-08-27 02:36:47 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-TOKEN = "8004127220:AAFa1SmmQIr0SCl7EvkgDTotA27JNBnvbA0"
-ADMIN_ID = 5168384940
+# config.py
+# Telegram Bot Token এবং Admin ID রাখার ফাইল
+
+TOKEN = "8004127220:AAFa1SmmQIr0SCl7EvkgDTotA27JNBnvbA0"   # এখানে BotFather থেকে পাওয়া টোকেন বসান
+ADMIN_ID = 5168384940           # আপনার Admin ID
